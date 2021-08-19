10bced32c8e0ecea2ee2a3346da6827f8be69809
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-08-19 07:29:25 -0300

    Fix lines

--- server/src/infra/web-sockets/index.ts
+++ server/src/infra/web-sockets/index.ts
@@ -3,17 +3,11 @@ import Teacher from '@app/core/entities/Teacher';
 
 export interface SocketServer {
   hasTeacher(teacherId: string): Promise<boolean>;
-
   teacherIsBusy(teacherId: string): Promise<boolean>;
-
   setTeacherAsBusy(teacherId: string): Promise<void>;
-
   requestTeacher(request: RequestLesson): Promise<void>;
-
   teachersIds(): Promise<string[]>;
-
   teachersIdsNotBusy(): Promise<string[]>;
-
   connectTeacher(teacher: Teacher): Promise<void>;
 }
 
