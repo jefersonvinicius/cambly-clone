cd4b2a52140c38ed2e5976dd99b1003fd0777e72
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-08-25 20:20:31 -0300

    Move SocketServer to own file

---
+++ server/src/infra/web-sockets/SocketServer.ts
@@ -0,0 +1,12 @@
+import RequestLesson from '@app/core/entities/RequestLesson';
+import Teacher from '@app/core/entities/Teacher';
+
+export default interface SocketServer {
+  hasTeacher(teacherId: string): Promise<boolean>;
+  teacherIsBusy(teacherId: string): Promise<boolean>;
+  setTeacherAsBusy(teacherId: string): Promise<void>;
+  requestTeacher(request: RequestLesson): Promise<void>;
+  teachersIds(): Promise<string[]>;
+  teachersIdsNotBusy(): Promise<string[]>;
+  connectTeacher(teacher: Teacher): Promise<void>;
+}
