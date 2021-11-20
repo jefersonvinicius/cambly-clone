cc9b0ee4150595b9e2041dd9f77ba447c0b1df57
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-11-20 16:31:49 -0300

    Add image attribute to User model

--- web-app/src/models/User.ts
+++ web-app/src/models/User.ts
@@ -1,9 +1,15 @@
 export class User {
   public name: string;
   public email: string;
+  private _image?: string;
 
   constructor(data: any) {
     this.name = data.name;
     this.email = data.email;
+    this._image = data.image;
+  }
+
+  get image() {
+    return this._image ?? "/assets/placeholder-avatar.png";
   }
 }
