b30865fb63f974839a79a1f5bb981a77e05c6cea
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-11-28 11:46:29 -0300

    Add bio field to User model

--- web-app/src/models/User.ts
+++ web-app/src/models/User.ts
@@ -1,11 +1,13 @@
 export class User {
   public name: string;
   public email: string;
+  public bio: string;
   private _image?: string;
 
   constructor(data: any) {
     this.name = data.name;
     this.email = data.email;
+    this.bio = data.bio;
     this._image = data.image;
   }
 
